7ebc36addfa324748d9e40504cedc1a62faa3669
psantos-duall <pedro.santos@duallstudio.com>
2023-03-23 15:57:24 +0000

    Update server.js

--- server.js
+++ server.js
@@ -14,13 +14,15 @@ const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-
 /*
+app.use(express.json());
+*/
+
+
 app.use(express.urlencoded({
     extended: true
 }));
-*/
+
 
 
 //console.log()
