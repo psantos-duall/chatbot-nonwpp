c782f5c68d44f67f3c6ebcd2b842dfc3783e0375
psantos-duall <pedro.santos@duallstudio.com>
2023-03-23 12:43:50 +0000

    Update server.js

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
     try {
+        console.log(req);
         console.log(req.body);
         
         const prompt = req.body.prompt;
